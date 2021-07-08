2b6f920e4d46c133638292aafb4e15b6987fa40b
Anorue Henry Osita <henry.anorue@decagon.dev>
2021-07-08 01:30:31 +0100

    Updated Assignment_ dict_comp.py

--- dict_comp.py
+++ dict_comp.py
@@ -6,14 +6,14 @@ def dict_comp(stop, step):
       floor_div = stop//step
       remainder = stop % step
       factor_of_4 = stop - remainder
-      lstx = list(range(1,factor_of_4+1))
+      list_of_range = list(range(1,factor_of_4+1))
       output2 =[f"items-{n}" for n in list(range(1,floor_div + 1))]
-      output1 = [lstx[i:i + step] for i in range(0, len(lstx), step) ]
+      output1 = [list_of_range[i:i + step] for i in range(0, len(list_of_range), step) ]
       counter = 0
       for iter in output2:
             dict[iter] = output1[counter]
             counter += 1
       print(dict)   
       
-dict_comp(3,8)   
+dict_comp(10, 4)   
   
